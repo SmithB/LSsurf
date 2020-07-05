ff912e53888899d51374829fc224d1423ddbfb32
Ben Smith <bsmith@apl.washington.edu>
2020-07-05 15:42:29 -0600

    read_ICESat2.py : extensive revisions

--- read_ICESat2.py
+++ read_ICESat2.py
@@ -7,18 +7,19 @@ Created on Tue Aug 20 15:00:05 2019
 """
 import numpy as np
 from PointDatabase.check_ATL06_blacklist import check_rgt_cycle_blacklist
-from PointDatabase.geo_index import geo_index
-from PointDatabase.point_data import point_data
+#from PointDatabase.geo_index import geo_index
+#from PointDatabase.point_data import point_data
 from PointDatabase.ATL06_filters import segDifferenceFilter
-from PointDatabase.ATL06_tiles import reconstruct_tracks
- 
+from PointDatabase.reconstruct_ATL06_tracks import reconstruct_ATL06_tracks
+import pointCollection as pc
+
 def read_ICESat2(xy0, W, gI_file, sensor=2, SRS_proj4=None, tiled=True, seg_diff_tol=2, blockmedian_scale=None, cplx_accept_threshold=0.):
     field_dict={None:['delta_time','h_li','h_li_sigma','latitude','longitude','atl06_quality_summary','segment_id','sigma_geo_h'], 
-                'fit_statistics':['dh_fit_dx', 'n_fit_photons','w_surface_window_final','snr_significance'],
+                'fit_statistics':['dh_fit_dx', 'dh_fit_dy', 'n_fit_photons','w_surface_window_final','snr_significance'],
+                'geophysical':['tide_ocean'],
                 'ground_track':['x_atc'],
-                'geophysical' : ['dac'],
                 'orbit_info':['rgt','cycle_number'],
-                'derived':['valid','matlab_time', 'BP','LR']}
+                'derived':['valid', 'BP','LR']}
     if tiled:
         fields=[]
         for key in field_dict:
@@ -27,12 +28,12 @@ def read_ICESat2(xy0, W, gI_file, sensor=2, SRS_proj4=None, tiled=True, seg_diff
     else:
         fields=field_dict
     px, py=np.meshgrid(np.arange(xy0[0]-W['x']/2, xy0[0]+W['x']/2+1.e4, 1.e4),np.arange(xy0[1]-W['y']/2, xy0[1]+W['y']/2+1.e4, 1.e4) )  
-    D0=geo_index().from_file(gI_file).query_xy((px.ravel(), py.ravel()), fields=fields)
+    D0=pc.geoIndex().from_file(gI_file).query_xy((px.ravel(), py.ravel()), fields=fields)
     if D0 is None or len(D0)==0:
         return [None]
     # check the D6 filenames against the blacklist of bad files
     if tiled:
-        D0=reconstruct_tracks(point_data(list_of_fields=fields).from_list(D0))
+        D0=reconstruct_ATL06_tracks(pc.data(fields=fields).from_list(D0))
     blacklist=None
     D1=list()
     for ind, D in enumerate(D0):
@@ -43,7 +44,7 @@ def read_ICESat2(xy0, W, gI_file, sensor=2, SRS_proj4=None, tiled=True, seg_diff
             D1.append(D)
 
     # D1 is now a filtered version of D0
-    D_pt=point_data().from_list(D1)
+    D_pt=pc.data().from_list(D1)
     bin_xy=1.e4*np.round((D_pt.x+1j*D_pt.y)/1.e4)
     cplx_bins=[]
     for xy0 in np.unique(bin_xy):
@@ -51,7 +52,7 @@ def read_ICESat2(xy0, W, gI_file, sensor=2, SRS_proj4=None, tiled=True, seg_diff
         if np.mean(D_pt.atl06_quality_summary[ii]==0) < cplx_accept_threshold:
             cplx_bins+=[xy0]
     cplx_bins=np.array(cplx_bins)
-
+    sigma_corr=np.zeros(len(D1))
     for ind, D in enumerate(D1):
         
         valid=segDifferenceFilter(D, setValid=False, toNaN=False, tol=seg_diff_tol)
@@ -69,29 +70,39 @@ def read_ICESat2(xy0, W, gI_file, sensor=2, SRS_proj4=None, tiled=True, seg_diff
         if blockmedian_scale is not None:
             D.blockmedian(blockmedian_scale, field='h_li')
               
-        # rename the h_li field to 'z', and rename the 'matlab_time' to 'day'
-        D.assign({'z': D.h_li+D.dac,'time':D.matlab_time,'sigma':D.h_li_sigma,'sigma_corr':D.sigma_geo_h,'cycle':D.cycle_number})
-        D.assign({'year':D.delta_time/24./3600./365.25+2018})
+        # rename the h_li field to 'z', and set time to the year
+        # note that the extra half day is needed because 2018 is in between two leap years
+        D.assign({'z': D.h_li, 'time':D.delta_time/24/3600/365.25+2018+0.5/365.25,
+                  'sigma':D.h_li_sigma,'cycle':D.cycle_number})
         # thin to 40 m
         D.index(np.mod(D.segment_id, 2)==0)  
-        if 'x' not in D.list_of_fields:
+        if 'x' not in D.fields:
             D.get_xy(SRS_proj4)
         #D.index(np.isfinite(D.h_li), list_of_fields=['x','y','z','time','year','sigma','sigma_corr','rgt','cycle'])
                 
         D.assign({'sensor':np.zeros_like(D.x)+sensor})
-        D1[ind]=D.subset(np.isfinite(D.h_li), datasets=['x','y','z','time','delta_time','year','sigma','sigma_corr','rgt','cycle','sensor', 'BP','LR'])
+        dhdy_med=np.nanmedian(D.dh_fit_dy)
+        dhdx_med=np.nanmedian(D.dh_fit_dx)
+        sigma_geo_x=8
+        sigma_corr[ind]=np.sqrt(0.03**2+sigma_geo_x**2*(dhdx_med**2+dhdy_med**2))
+        if ~np.isfinite(sigma_corr[ind]):
+            sigma_corr[ind]=0.1
+        D.assign({'sigma_corr':np.zeros_like(D.h_li)+sigma_corr[ind]})
+        D1[ind]=D.copy_subset(np.isfinite(D.h_li), datasets=['x','y','z','time',\
+                 'delta_time','sigma','sigma_corr','rgt','cycle',\
+                     'sensor', 'BP','LR'])
     return D1
 
 def main():
-    gI_file='/Volumes/insar10/ben/IS2_tiles/GL/GeoIndex.h5'
+    import glob
+    #gI_file='/Volumes/insar10/ben/IS2_tiles/GL/GeoIndex.h5'
+    gI_file=glob.glob('/Volumes/ice2/ben/scf/GL_06/latest/tiles/*/GeoIndex.h5')[0]
     xy0=[-170000.0, -2280000.0]
-    dI=point_data().from_list(read_ICESat2(xy0, {'x':2.e4, 'y':2.e4}, gI_file, cplx_accept_threshold=0.25))
+    dI=pc.data().from_list(read_ICESat2(xy0, {'x':4.e4, 'y':4.e4}, gI_file, cplx_accept_threshold=0.25))
     import matplotlib.pyplot as plt
-    #plt.plot(dI.x, dI.y,'ro')
-    #dI=dI=point_data().from_list(read_ICESat2(xy0, {'x':2.e4, 'y':2.e4}, gI_file, cplx_accept_threshold=0))
-    #plt.plot(dI.x, dI.y,'kx')
-    #plt.axis('equal')
+
     plt.scatter(dI.x, dI.y, c=dI.z, linewidth=0); plt.colorbar()
+    plt.axis('equal')
     
     
 if __name__=='__main__':

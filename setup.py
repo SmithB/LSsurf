a828f737ca12d8e3e0cce80e453bec0c07f25672
smithB <bsmith@apl.washington.edu>
2020-07-06 14:45:59 -0700

    Make package installible

--- setup.py
+++ setup.py
@@ -1,9 +1,33 @@
 from setuptools import setup
 from Cython.Build import cythonize
 import numpy
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="spsolve_tr_upper",
-    ext_modules=cythonize("spsolve_tr_upper.pyx"),
+    name='LSsurf', 
+    version='1.0.0.0',
+    description='Utilities for fitting smooth surfaces to point data', 
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/SmithB/LSsurf',
+    author='Ben Smith',
+    author_email='besmith@uw.edu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(),
+
+    ext_modules=cythonize("LSsurf/*.pyx"),
     include_dirs=[numpy.get_include()],
     zip_safe=False
 )

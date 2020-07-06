bf220b68583ba6444da7ff0f0470dde67d28fba4
smithB <bsmith@apl.washington.edu>
2020-07-06 14:48:19 -0700

    more work on making installible

--- LSsurf/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy
-setup(
-    name="spsolve_tr_upper",
-    ext_modules=cythonize("spsolve_tr_upper.pyx"),
-    include_dirs=[numpy.get_include()],
-    zip_safe=False
-)

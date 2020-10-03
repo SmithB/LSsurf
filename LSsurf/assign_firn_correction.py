4df2d4170e8bf7035ee9a31792dca67aa8becd8a
Ben Smith <bsmith@apl.washington.edu>
2020-10-02 21:46:33 -0600

    LSsurf/assign_firn_correction.py: updated to module version of routines

--- LSsurf/assign_firn_correction.py
+++ LSsurf/assign_firn_correction.py
@@ -7,37 +7,52 @@ Created on Thu Aug 22 14:08:42 2019
 """
 
 import numpy as np
-from LSsurf.racmo_interp_firn_height import interpolate_racmo_firn
-from LSsurf.racmo_extrap_firn_height import extrapolate_racmo_firn
-from LSsurf.racmo_interp_downscaled import interpolate_racmo_downscaled
-from LSsurf.racmo_extrap_downscaled import extrapolate_racmo_downscaled
+import SMBcorr
+#from LSsurf.racmo_interp_firn_height import interpolate_racmo_firn
+#from LSsurf.racmo_extrap_firn_height import extrapolate_racmo_firn
+#from LSsurf.racmo_interp_downscaled import interpolate_racmo_downscaled
+#from LSsurf.racmo_extrap_downscaled import extrapolate_racmo_downscaled
 
 #from LSsurf.interp_MAR_firn import interp_MAR_firn
 
-def assign_firn_correction(data, firn_correction, hemisphere, subset_valid=True):
+def assign_firn_correction(data, firn_correction, hemisphere, subset_valid=True, EXTRAPOLATE=False):
     # if we're rereading data, it already has the firn correction applied
     if firn_correction == 'MAR':
         if hemisphere==1:
-            data.assign({'h_firn':interp_MAR_firn(data.x, data.y, data.time)})
+            data.assign({'h_firn':SMBcorr.interp_MAR_firn(data.x, data.y, data.time)})
             data.z -= data.h_firn
+    elif firn_correction == 'MERRA2_hybrid':
+        if hemisphere==-1:
+            FAC = SMBcorr.interpolate_merra_hybrid('/Volumes/ice1/tyler/MERRA2_hybrid/v1', "EPSG:3031",
+                            'ais', data.time, data.x, data.y,
+                            VERSION='v1', VARIABLE='FAC',
+                            SIGMA=1.5, FILL_VALUE=np.nan)
+            bad = FAC.mask
+            if np.any(bad):
+                FAC[bad] = SMBcorr.extrapolate_merra_hybrid('/Volumes/ice1/tyler/MERRA2_hybrid/v1', "EPSG:3031",
+                            'ais', data.time[bad], data.x[bad], data.y[bad],
+                            VERSION='v1', VARIABLE='FAC',
+                            SIGMA=1.5, FILL_VALUE=np.nan, EXTRAPOLATE=EXTRAPOLATE)
+            data.assign({'h_firn':FAC})
+
     elif firn_correction == 'RACMO':
         if hemisphere==1:
-            h_firn=interpolate_racmo_firn('/Volumes/ice1/tyler', "EPSG:3413", 'FGRN055', data.time, data.x, data.y, VARIABLE='zs', FILL_VALUE=np.NaN)[0]
+            h_firn=SMBcorr.interpolate_racmo_firn('/Volumes/ice1/tyler', "EPSG:3413", 'FGRN055', data.time, data.x, data.y, VARIABLE='zs', FILL_VALUE=np.NaN)[0]
             # check if any data points are over "unglaciated" values
             bad=~np.isfinite(h_firn)
             if np.any(bad):
                 # extrapolate model to coordinates
-                h_firn[bad]=extrapolate_racmo_firn('/Volumes/ice1/tyler', "EPSG:3413", 'FGRN055', data.time[bad], data.x[bad], data.y[bad], VARIABLE='zs', FILL_VALUE=np.NaN)[0]
+                h_firn[bad]=SMBcorr.extrapolate_racmo_firn('/Volumes/ice1/tyler', "EPSG:3413", 'FGRN055', data.time[bad], data.x[bad], data.y[bad], VARIABLE='zs', FILL_VALUE=np.NaN)[0]
             data.assign({'h_firn':h_firn})
             data.z -= data.h_firn
     elif firn_correction in ["RACMO_fac", "RACMO_fac_smb", "RACMO_smb"]:
         if hemisphere==1 and firn_correction in ["RACMO_fac","RACMO_fac_smb"]:
-            h_fac = interpolate_racmo_firn('/Volumes/ice1/tyler', "EPSG:3413", 'FGRN055', data.time, data.x, data.y, VARIABLE='FirnAir', FILL_VALUE=np.NaN)[0]
+            h_fac = SMBcorr.interpolate_racmo_firn('/Volumes/ice1/tyler', "EPSG:3413", 'FGRN055', data.time, data.x, data.y, VARIABLE='FirnAir', FILL_VALUE=np.NaN)[0]
             # check if any data points are over "unglaciated" values
             bad=~np.isfinite(h_fac)
             if np.any(bad):
                 # extrapolate model to coordinates
-                h_fac[bad]=extrapolate_racmo_firn('/Volumes/ice1/tyler', "EPSG:3413", 'FGRN055', data.time[bad], data.x[bad], data.y[bad], VARIABLE='FirnAir', FILL_VALUE=np.NaN)[0]
+                h_fac[bad]=SMBcorr.extrapolate_racmo_firn('/Volumes/ice1/tyler', "EPSG:3413", 'FGRN055', data.time[bad], data.x[bad], data.y[bad], VARIABLE='FirnAir', FILL_VALUE=np.NaN)[0]
             data.z -= h_fac
             data.assign({'fac':h_fac})
         else:
@@ -46,7 +61,7 @@ def assign_firn_correction(data, firn_correction, hemisphere, subset_valid=True)
             # ice density used in RACMO models (kg/m^3)
             rho_model = 910.0
             if hemisphere==1:
-                smb = interpolate_racmo_downscaled('/Volumes/ice1/tyler', "EPSG:3413", '3.0', 'SMB', data.time, data.x, data.y, FILL_VALUE=np.NaN)[0]
+                smb = SMBcorr.interpolate_racmo_downscaled('/Volumes/ice1/tyler', "EPSG:3413", '3.0', 'SMB', data.time, data.x, data.y, FILL_VALUE=np.NaN)[0]
                 # check if any data points are over invalid downscaled values
                 bad=~np.isfinite(smb)
                 #if np.any(bad):
@@ -60,12 +75,12 @@ def assign_firn_correction(data, firn_correction, hemisphere, subset_valid=True)
 
 def main():
     import matplotlib.pyplot as plt
-    from PointDatabase import point_data
+    import pointCollection as pc
     t=np.arange(2002, 2022, 0.1)
     xy_jako=(-170000, -2280000)
     corrections = ['RACMO_fac', 'RACMO_fac_smb', 'RACMO']
     for correction in corrections:
-        data=point_data().from_dict({'time':t, 'x':np.ones_like(t)*xy_jako[0], 'y':np.ones_like(t)*xy_jako[1], 'z':np.zeros_like(t)})
+        data=pc.data().from_dict({'time':t, 'x':np.ones_like(t)*xy_jako[0], 'y':np.ones_like(t)*xy_jako[1], 'z':np.zeros_like(t)})
         assign_firn_correction(data, correction, 1)
         plt.plot(data.time, data.z)
     plt.legend(corrections)

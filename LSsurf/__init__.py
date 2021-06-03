82f92cce8b678f50e393a7b2a2e24e597b7a78f8
Ben Smith <besmith@uw.edu>
2021-06-03 01:06:23 -0400

    updated __init__.py

--- LSsurf/__init__.py
+++ LSsurf/__init__.py
@@ -5,4 +5,5 @@ from LSsurf.lin_op import lin_op
 from LSsurf.matlab_to_year import matlab_to_year
 from LSsurf.smooth_xyt_fit import smooth_xyt_fit
 from LSsurf.smooth_xytb_fit import smooth_xytb_fit
+from LSsurf.setup_grid_bias import setup_grid_bias
 from LSsurf.unique_by_rows import unique_by_rows

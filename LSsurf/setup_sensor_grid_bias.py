f2ed094c4c88ee493e71547b95c68bfc1ea2e040
Ben Smith <bsmith@apl.washington.edu>
2022-04-25 09:49:41 -0700

    LSsurf/setup_sensor_grid_bias.py: Catch no-prior-nodes-in-grid errors, change temporary variable names

--- LSsurf/setup_sensor_grid_bias.py
+++ LSsurf/setup_sensor_grid_bias.py
@@ -1,9 +1,10 @@
 from LSsurf import lin_op, fd_grid
 import numpy as np
 
-def setup_sensor_grid_bias(sensor, data, G_data, constraint_op_list, grids, \
+def setup_sensor_grid_bias(data, grids, G_data, constraint_op_list,\
+                    sensor=None,\
                      spacing=None, expected_rms=None, \
-                     expected_value=0, expected_rms_grad=None ):
+                     expected_val=0, expected_rms_grad2=None ):
     '''
     set up a matrix to fit a gridded bias that multiplies a data parameter
     
@@ -16,28 +17,33 @@ def setup_sensor_grid_bias(sensor, data, G_data, constraint_op_list, grids, \
         expected_value: float, default=0: Expected value of the parameter.
         expected_rms_grad: float, default=None: expected RMS gradient of the parameter.  If not specified, the gradient of the parameter will be unconstrained
     '''
-    Dsub=data[data.sensor==sensor]
+    sensor_rows=np.flatnonzero(data.sensor==sensor)
+    Dsub=data[sensor_rows]
     name = f'sensor_{sensor}_bias'
-    xr=[np.floor(np.min(Dsub.x)/spacing)*spacing, np.ceil(np.max(Dsub.x/spacing))*spacing]
-    yr=[np.floor(np.min(Dsub.x)/spacing)*spacing, np.ceil(np.max(Dsub.y/spacing))*spacing]
-    grid=fd_grid([yr, xr], spacing*np.ones(2), name=name)
+    try:
+        xr=[np.floor(np.min(Dsub.x)/spacing)*spacing, np.ceil(np.max(Dsub.x/spacing))*spacing]
+        yr=[np.floor(np.min(Dsub.y)/spacing)*spacing, np.ceil(np.max(Dsub.y/spacing))*spacing]
+        grid=fd_grid([yr, xr], spacing*np.ones(2), name=name)
+    except (IndexError, ValueError):
+        # grid contains no nodes
+        return
     grids[name]=grid
     grid.col_0 = G_data.col_N
     grid.col_N = grid.col_0 + grid.N_nodes
     interp_mtx=lin_op(grid=grid, name=name).\
             interp_mtx(Dsub.coords()[0:2])
+    interp_mtx.r=sensor_rows[interp_mtx.r.ravel()].reshape(interp_mtx.r.shape)
     G_data.add(interp_mtx)
     #Build a constraint matrix for the curvature of the bias
-    if expected_rms_grad is not None:
-        grad2_param=lin_op(grid, name='grad2_'+name).grad2(DOF=param)
-        grad2_param.expected=expected_rms_grad+np.zeros(grad2_param.N_eq)/\
-            np.sqrt(np.prod(grid.delta))
-        constraint_op_list.append(grad2_param)
+    if expected_rms_grad2 is not None:
+        grad2_b=lin_op(grid, name='grad2_'+name).grad2(DOF=name)
+        grad2_b.expected=expected_rms_grad2/np.sqrt(np.prod(grid.delta))+np.zeros(grad2_b.N_eq)
+        constraint_op_list.append(grad2_b)
     #Build a constraint matrix for the magnitude of the bias
     if expected_rms is not None:
-        mag_param=lin_op(grid, name='mag_'+name).data_bias(\
+        mag_b=lin_op(grid, name='mag_'+name).data_bias(\
                 ind=np.arange(grid.N_nodes),
                 col=np.arange(grid.col_0, grid.col_N))
-        mag_param.expected=expected_rms+np.zeros(mag_param.N_eq)
-        mag_param.expected_val=expected_value+np.zeros(mag_param.N_eq)
-        constraint_op_list.append(mag_param)
+        mag_b.expected=expected_rms+np.zeros(mag_b.N_eq)
+        mag_b.prior=expected_val+np.zeros(mag_b.N_eq)
+        constraint_op_list.append(mag_b)

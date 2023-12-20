52aab3aac014bc3d982bdbada55415f6c9649aca
Ben Smith <bsmith@apl.washington.edu>
2023-12-19 16:06:14 -0800

    LSsurf/data_slope_bias.py: make the rescaling of the data slope bias a named variable, match rescaling of constraint equations to rescaling of operator

--- LSsurf/data_slope_bias.py
+++ LSsurf/data_slope_bias.py
@@ -12,11 +12,11 @@ def data_slope_bias(data,  bias_model, col_0=0, sensors=[], op_name='data_slope'
                     E_rms_bias=1.e-5):
     """
         Make a matrix that adds a set of parameters representing the biases of a set of data.
-        
-        Note that units of the x and y coordinates of the fit are km, so the 
-        slopes are in m/km.  This helps preserve the scaling of the fit matrix, 
+
+        Note that units of the x and y coordinates of the fit are km, so the
+        slopes are in m/km.  This helps preserve the scaling of the fit matrix,
         which otherwise would have values of tens of thousands
-        
+
         input arguments:
              data: data for the problem.  Must contain parameters 'x' and 'y'
              bias_model: bias_model dict from assign_bias_params
@@ -39,7 +39,9 @@ def data_slope_bias(data,  bias_model, col_0=0, sensors=[], op_name='data_slope'
 
     col_N=col_0+2*len(these_sensors)
     rr, cc, vv=[[], [], []]
-    
+
+    rescale=1000
+
     for d_col_1, sensor in enumerate(these_sensors):
         rows=np.flatnonzero(data.sensor==sensor)
         bias_model['slope_bias_dict'][sensor]=col_0+d_col_1*2+np.array([0, 1])
@@ -48,7 +50,7 @@ def data_slope_bias(data,  bias_model, col_0=0, sensors=[], op_name='data_slope'
             delta -= np.nanmean(delta)
             rr += [rows]
             cc += [np.zeros_like(rows, dtype=int) + int(col_0+d_col_1*2+d_col_2)]
-            vv += [delta/1000]
+            vv += [delta/rescale]
 
     G_bias = lin_op(col_0=col_0, col_N=col_N, name=op_name)
     G_bias.r = np.concatenate(rr)
@@ -57,7 +59,7 @@ def data_slope_bias(data,  bias_model, col_0=0, sensors=[], op_name='data_slope'
 
     ii=np.arange(col_0, col_N, dtype=int)
     Gc_bias=lin_op(name='constraint_'+op_name, col_0=col_0, col_N=col_N).data_bias(ii-ii[0], col=ii)
-    Gc_bias.expected = np.zeros(Gc_bias.shape[0])+E_rms_bias
+    Gc_bias.expected = np.zeros(Gc_bias.shape[0])+E_rms_bias*rescale
     Gc_bias.prior = np.zeros(Gc_bias.shape[0])
 
     return G_bias, Gc_bias, bias_model

c12817e0fa5fa9e690a77071b6b8e352f1c83721
Ben Smith <bsmith@apl.washington.edu>
2024-05-03 14:56:22 -0700

    LSsurf/__init__.py : make smooth_fit and iterate_fit package functions

--- LSsurf/__init__.py
+++ LSsurf/__init__.py
@@ -3,6 +3,8 @@ from LSsurf.data_slope_bias import data_slope_bias
 from LSsurf.fd_grid import fd_grid
 from LSsurf.lin_op import lin_op
 from LSsurf.matlab_to_year import matlab_to_year
+from LSsurf.smooth_fit import smooth_fit
+from LSsurf.smooth_fit import iterate_fit
 from LSsurf.smooth_xyt_fit import smooth_xyt_fit
 from LSsurf.smooth_xytb_fit import smooth_xytb_fit
 from LSsurf.setup_grid_bias import setup_grid_bias

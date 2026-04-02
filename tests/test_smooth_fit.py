2c6a7ac2d37ff41497ab86d68ae44f59a374ef6b
Ben Smith <bsmith@apl.washington.edu>
2026-04-02 12:04:17 -0700

    added test functions for top-level smooth_fit function

---
+++ tests/test_smooth_fit.py
@@ -0,0 +1,93 @@
+import numpy as np
+from LSsurf.smooth_fit import smooth_fit
+import pointCollection as pc
+import os
+
+
+def run_fit():
+
+    # define the domain's width in x, y, and time
+    W = {'x': 1.e4, 'y': 400, 't': 2}
+    # define the grid center:
+    ctr = {'x': 0., 'y': 0., 't': 0.}
+    # define the grid spacing
+    spacing = {'z0': 100, 'dz': 200, 'dt': 0.25}
+
+    # define the data as a sine wave with a wavelength of 2 km and an amplitude of 100m.
+    x = np.arange(-W['x']/2, W['x']/2, 100)
+    lambda_x = 2000
+    amp = 100
+
+    D0 = pc.data().from_dict({'x': x, 'y': np.zeros_like(x), 'z': np.zeros_like(x),
+                              'time': np.zeros_like(x)-0.99, 'sigma': np.zeros_like(x)+1})
+    D1 = pc.data().from_dict({'x': x, 'y': np.zeros_like(x), 'z': amp-amp*np.cos(2*np.pi*x/lambda_x),
+                              'time': np.zeros_like(x)+0.99, 'sigma': np.zeros_like(x)+1})
+    data_dt = pc.data().from_list([D0, D1])
+
+    data_gap_scale = 2500
+
+    E_RMS = {}  # 'd2z0_dx2':E_d2z0dx2, 'dz0_dx':E_d2z0dx2*data_gap_scale, 'd3z_dx2dt':E_d3zdx2dt, 'd2z_dxdt':E_d3zdx2dt*data_gap_scale,  'd2z_dt2':E_d2zdt2}
+
+    E_RMS['d3z_dx2dt'] = 0.006
+    E_RMS['d2z_dxdt'] = 0.006*data_gap_scale
+    E_RMS['d2z0_dx2'] = 0.03
+    E_RMS['dz0_dx'] = 0.03*data_gap_scale
+    E_RMS['d2z_dt2'] = 6000
+
+    data_dt_gap = data_dt[np.abs(data_dt.x) > 1000]
+    W1 = {'x': 10000.0, 'y': 400, 't': 2}
+    S = smooth_fit(data=data_dt_gap, ctr=ctr, W=W1, spacing=spacing, E_RMS=E_RMS,
+                   reference_epoch=4,  compute_E=True,
+                   max_iterations=1, dzdt_lags=[1])
+    return S
+
+
+def save_fit():
+
+    # this function will replace the output of the test function if it does not exist.
+    # After this happens the tests will succeed in an unenlightening way.
+
+    # check if the output files exist.
+    file_count = 0
+    for group in ['z0', 'dz']:
+        dst_file = os.path.join('test_data', group+'.h5')
+        if os.path.isfile(dst_file):
+            file_count += 1
+
+    # if all files exist, return
+    if file_count == 2:
+        return
+
+    print("regenerating the fit:")
+    S = run_fit()
+
+    if not os.path.isdir('test_data'):
+        os.mkdir('test_data')
+
+    for group in ['z0', 'dz']:
+        dst_file = os.path.join('test_data', group+'.h5')
+        S['m'][group].to_h5(dst_file)
+
+    return
+
+
+def test_fit():
+
+    # refresh the saved files if needed:
+    save_fit()
+
+    # run the fit:
+    S = run_fit()
+
+    # load the saved data
+    m_ref = {name: pc.data().from_h5(os.path.join('test_data', name+'.h5'))
+             for name in ['z0', 'dz']}
+
+    # make sure the saved data matches the fit model
+    for group, ref_data in m_ref.items():
+        test_data = S['m'][group]
+        for field in m_ref[group].fields:
+            assert np.allclose(getattr(test_data, field),
+                               getattr(ref_data, field),
+                               equal_nan=True, atol=0.01, rtol=0.1), \
+                f"excessive misfit for field {field} in group {group}"
